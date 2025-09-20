cc19445b5efbd176da700ef057a5bd745363da48
Deepak-2514 <kjdeepak2514@gmail.com>
2025-09-20 21:47:36 +0530

    added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import Search from "./components/Search";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-300">
-        {" "}
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner"></img>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without The Hassle
+            </h1>
+            <Search />
+          </header>
+        </div>
+      </div>
+    </main>
   );
 };
 
